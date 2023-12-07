2c0923e05dcde6561458302fe5309dc465ee6ab8
KajetanMieloch <91525250+KajetanMieloch@users.noreply.github.com>
2023-12-07 10:10:21 +0100

    Add info and status
    Now we know the bot is configured correctly

--- botTest.py
+++ botTest.py
@@ -1,4 +1,4 @@
-from binance.client import Client
+from binance import Client
 from dotenv import load_dotenv
 import os
 
@@ -8,3 +8,9 @@ api_key = os.getenv('KEY')
 api_secret = os.getenv('SECRET')
 
 client = Client(api_key, api_secret, testnet=True)
+
+status = client.get_system_status()
+info = client.get_symbol_info('BTCUSDT')
+
+print(status)
+print(info)
